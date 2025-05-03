84e65495d435f3265717199c37860db572d237fb
Sankara Subramanian <gsankarasubramaniann@gmail.com>
2025-05-03 10:38:22 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -13,7 +13,7 @@ const AdminContextProvider = (props) => {
     const [appointments,setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
     
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://pallava-backend.onrender.com"
 
     const getAllDoctors = async () => {
         try {
